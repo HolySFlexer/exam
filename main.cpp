46274a0e33651075bb7f537b3158862e8dd9507d
HolySFlexer <138931349+HolySFlexer@users.noreply.github.com>
2023-09-12 20:24:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -37,12 +37,59 @@ void DeleteList(Node* head) {
 	}
 }
 
+//Удаление отрицательных чисел
 
+void DeleteOtric(Node* &head) {
+	while (head != nullptr && head->Data < 0) {
+		Node* tmp = head;
+		head = head->next;
+		delete tmp;
+	}
+	if (head == nullptr) return;
+	Node* curr = head->next;
+	Node* prev = head;
+	while (curr != nullptr) {
+		if (curr->Data < 0) {
+			prev->next = curr->next;
+			Node* tmp = curr;
+			curr = curr->next;
+			delete tmp;
+		}
+		else {
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+}
+
+//перемещение головы в конец 
+
+void Peremesheniye(Node*& head) {
+	Node* tmp = head;
+	int s = head->Data;
+	head = head->next;
+	delete tmp;
+	Node* curr = head;
+	while (curr->next!= nullptr) {
+		curr = curr->next;
+	}
+	curr->next = new Node;
+	curr = curr->next;
+	curr->Data = s;
+
+
+}
 int main() {
 	Node* L1;
 	int n = 0;
 	cin >> n;
 	L1=CreateList(n);
+
+	Peremesheniye(L1);
+
 	PrintList(L1);
-	DeleteList(L1);
 }
+
+
+
+
